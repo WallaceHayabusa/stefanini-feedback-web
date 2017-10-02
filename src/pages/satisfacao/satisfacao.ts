0c1260d479ae4604e431309dba73f13121a04474
Wilsley Guilherme Castro Silva Da Fonseca <wgfonseca@stefanini.com>
2017-10-02 18:38:12 -0300

    satisfacao do cliente

--- src/pages/satisfacao/satisfacao.ts
+++ src/pages/satisfacao/satisfacao.ts
@@ -5,5 +5,19 @@ import { Component } from '@angular/core';
     templateUrl: './satisfacao.html'
 })
 export class SatisfacaoPage {
+    heightBB = 0;
+    heightPF = 0;
+    heightCC = 0;
+    isCC: boolean;
+    constructor(){
 
+    }
+
+    correios(heightScroll: number){
+        if(this.heightCC == 0){
+            this.heightCC = heightScroll; 
+        }else{
+            this.heightCC = 0;
+        }
+    }
 }
